fab942da368b7c39c5c6b87ada4da6d7155485fb
tnauss <tnauss@localhost>
2011-11-25 15:37:15 +0100

    Rename: Rename some files and folders.

---
+++ src/julendat/scripts/stations_ki/compute_level_005_file.R
@@ -0,0 +1,12 @@
+source("/home/dogbert/Desktop/development/julendat/src/julendat/scripts/stations_ki/ComputeLevel005File.R")
+compute_level_005_file (
+asciipath="/home/dogbert/Desktop/development/test/processing/plots/ki/0000cof3/ra01_nai05_0000/ki_0000cof3_000rug_201102221025_201104180220_mez_ra01_nai05_0000.asc",
+outpath="/home/dogbert/Desktop/development/test/processing/plots/ki/0000cof3/ca01_nai05_0005/ki_0000cof3_000rug_201102221225_201104180420_eat_ca01_nai05_0005.dat",
+plotID="cof3",
+loggertype="000rug",
+cf=c(NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN),
+reorder=c(1,2,4,5),
+skip=5,
+quality=0005,
+adjust_time=7200,
+order_out=c("dateEAT", "plotID", "logger","t_rug", "rh_rug","precip_pu1", "p_precip_pu1", "c_precip_pu1","precip_pu2", "p_precip_pu2","c_precip_pu2","fog_pu2", "p_fog_pu2", "c_fog_pu2","t_wxt", "st_wxt", "rh_wxt","precip_wxt", "ws_wxt", "wd_wxt", "p_wxt","kd_w_wxt", "u_kd_wxt", "c_kd_wxt","ku_w_wxt", "u_ku_wxt", "c_ku_wxt","ld_w_wxt", "u_ld_wxt", "c_ld_wxt","lu_w_wxt", "u_lu_wxt", "c_lu_wxt", "t_cnr_wxt","kd_w_rad", "u_kd_rad", "c_kd_rad","par_m_rad", "u_par_rad", "c_par_rad", "t_rad", "rh_rad","chdateEAT", "year", "month", "day", "hour", "minute", "quality")) 
---
+++ src/julendat/scripts/stations_ki/init_level_010_file.R
@@ -0,0 +1,6 @@
+source("/home/dogbert/Desktop/development/julendat/src/julendat/scripts/stations_ki/InitLevel010File.R")
+init_level_010_file (
+outpath="/home/dogbert/Desktop/development/test/processing/plots/ki/0000cof3/ca01_nai05_0010/ki_0000cof3_000rug_201103010000_201103312355_eat_ca01_nai05_0010.dat",
+start_time="20110301000000",
+end_time="20110331235501",
+300.0)
