68f0e376f8b0873798363d6b1f211df62dcae78a
tnauss <tnauss@localhost>
2012-02-10 14:17:07 +0100

    Update: Add visualization for station data overview (manual version).

---
+++ src/julendat/rmodules/VColList.R
@@ -0,0 +1,8 @@
+colList <- list(colTa = colorRampPalette(c("darkblue", "darkseagreen",
+                        "gold", "darkred"), interpolate = "linear"),
+                colRH = colorRampPalette(c("tan4", "sandybrown", "white", 
+                        "lightsteelblue", "royalblue4"),
+                        interpolate = "linear"),
+                colPrec = colorRampPalette(c("white", "darkblue", 
+                          "mediumseagreen", "gold", "red", "purple", "black"),
+                          interpolate = "linear")) 
---
+++ src/julendat/rmodules/run_VStationData.R
@@ -0,0 +1,47 @@
+source("VStationData.R")
+source("VColList.R")
+#inpath <- "D:/development/test/julendat/processing/plots/be"
+inpath <- "/media/permanent/development/test/julendat/processing/plots/be"
+
+verbose <- T
+
+## list all file in input path (and sub folders) justifying pattern definition
+files <- list.files(inpath, recursive = T, pattern = "mez_ca05_nai30_0050.dat")
+files <- grep("_2008", files, value = T)
+print("HALLO")
+print(files)
+## read all files into a list
+ldat <- lapply(1:length(files), function(i) {
+  read.table(paste(inpath,files[i],sep="/"),header = T, sep = ",", fill = T)
+})
+
+## remove data from data list if no plot id is available
+ldat <- lapply(seq(ldat), function(i) subset(ldat[[i]], ldat[[i]]$PlotId != ""))
+
+## set data values used for visualization
+datetime <- lapply(seq(ldat), function(i) ldat[[i]]$Datetime)
+plotID <- lapply(seq(ldat), function(i) ldat[[i]]$PlotId)
+plotval <- lapply(seq(ldat), function(i) ldat[[i]]$Ta_200)
+
+## combine data list entries
+datetime <- unlist(datetime)
+plotID <- unlist(plotID)
+plotID <- substr(plotID, 1, 8)
+plotval <- unlist(plotval)
+plotval <- ifelse(plotval > 50, NA, ifelse(plotval < -20, NA, plotval))
+
+plotname <- paste("overview", "Ta_200", Sys.Date(), "wide", sep = "_")
+plotname <- paste(plotname, ".png", sep = "")
+
+## call function
+png(paste(inpath, plotname, sep = "/"), width = 1024*3, height = 768*3)
+VStationData(
+  plotval = plotval,         # x = t
+  datetime = datetime,       # date = datetime
+  cond = plotID,          # cond = plot
+  fun = mean,                # fun = mean
+  arrange = "long",          # arrange = "long"
+  main = "Temperature",      # main = "Temperature"
+  colour = colList$colPrec  # colour = colList$colPrec
+  )
+dev.off()
