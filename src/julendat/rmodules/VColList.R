958e8bf0ad8617ab183b46e24cecda2df4d9b279
Thomas Nauss <nausst@gmail.com>
2012-05-31 17:11:42 +0200

    Update: Add visualization for level 0050 datasets.

--- src/julendat/rmodules/VColList.R
+++ src/julendat/rmodules/VColList.R
@@ -1,8 +1,28 @@
-colList <- list(colTa = colorRampPalette(c("darkblue", "aquamarine",
-                        "gold", "darkred"), interpolate = "linear"),
-                colRH = colorRampPalette(c("tan4", "sandybrown", "white", 
-                        "lightsteelblue", "royalblue4"),
+VColList <- list(
+  Ta_200 = colorRampPalette(c("darkblue", "aquamarine", 
+                             "gold", "red2"),   interpolate = "linear"),
+  
+  rH_200 = colorRampPalette(c("tan4", "sandybrown", "white", "lightsteelblue", 
+                             "royalblue4"), interpolate = "linear"),
+  
+  Ts_10 = colorRampPalette(c("darkblue", "aquamarine", 
+                             "gold", "red2"),  interpolate = "linear"),
+  
+  SWDR_300 = colorRampPalette(c("black", "white"), interpolate = "linear"),
+  
+  SWUR_300 = colorRampPalette(c("black", "white"), interpolate = "linear"),
+  
+  LWDR_300 = colorRampPalette(c("black", "white"), interpolate = "linear"),
+  
+  LWUR_300 = colorRampPalette(c("black", "white"), interpolate = "linear"),
+  
+  WD = colorRampPalette(c("red2", "darkblue", "darkgreen", 
+                          "gold", "red2"),  interpolate = "linear"),
+  
+  WV = colorRampPalette(c("white", "lightblue", "cornflowerblue", "red2"), 
                         interpolate = "linear"),
-                colPrec = colorRampPalette(c("white", "darkblue", 
-                          "mediumseagreen", "gold", "red", "purple", "black"),
-                          interpolate = "linear")) 
+  
+  p_200 = colorRampPalette(c("white", "black"), interpolate = "linear"),
+  
+  P_RT_NRT = colorRampPalette(c("white", "darkblue", "aquamarine", "gold", "red", 
+                               "purple", "black"), interpolate = "linear"))
