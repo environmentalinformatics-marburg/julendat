2e4446fa304296d8549a73dd1529f141f5aff04a
tnauss <tnauss@localhost>
2012-11-19 16:25:23 +0100

    Update: Change date (just a test).

--- src/julendat/scripts/stations_ki/ki_process_uitfstation_level0000_gui.py
+++ src/julendat/scripts/stations_ki/ki_process_uitfstation_level0000_gui.py
@@ -19,7 +19,7 @@ reports to nausst@googlemail.com
 """
 
 __author__ = "Thomas Nauss <nausst@googlemail.com>, Tim Appelhans"
-__version__ = "2012-09-19"
+__version__ = "2012-11-19"
 __license__ = "GNU GPL, see http://www.gnu.org/licenses/"
 
 import ConfigParser
