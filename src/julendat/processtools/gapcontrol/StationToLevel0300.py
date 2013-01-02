cb331f225cf45317c02d47d438c6dbdedf06aa7c
tnauss <tnauss@localhost>
2013-01-02 14:21:08 +0100

    Update: Fix bugs in gap control.

--- src/julendat/processtools/gapcontrol/StationToLevel0300.py
+++ src/julendat/processtools/gapcontrol/StationToLevel0300.py
@@ -195,6 +195,6 @@ class StationToLevel0300:
         f.write(r_cmd)
         f.close()
         r_cmd = 'R CMD BATCH ' + r_script  + ' ' + r_script + '.log'
-        #os.system(r_cmd)
+        os.system(r_cmd)
         sys.exit()
         os.chdir(act_wd)
