03b88e3d7a4d9893d9069ac0e513c0257a3494f1
tnauss <tnauss@localhost>
2013-01-12 08:27:07 +0100

    Update: Include gap filling for wxt depending also on rug data.

--- src/julendat/processtools/vis/VISLevel0050.py
+++ src/julendat/processtools/vis/VISLevel0050.py
@@ -145,9 +145,19 @@ class VISLevel0050:
         self.main()
 
     def main(self):
-        """Processes level 0000 station files to level 0050.
-        """
         
+        if "wxt" in self.loggers:
+            loggers = self.loggers 
+            self.loggers = ["wxt"]
+            self.visualize_wxt()
+            self.loggers = loggers
+            self.loggers.remove('wxt')            
+
+        self.visualize_stations()
+        
+    def visualize_stations(self):
+        """Visualize station datasets
+        """
         os.chdir(self.r_filepath)
         r_source = 'source("print.ki.strip.R")'
         r_script = 'print.ki.strip('
@@ -155,20 +165,22 @@ class VISLevel0050:
         r_arrange = 'arrange = "long",'
         r_pattern = 'pattern  = "' + self.pattern + '",'
         r_year = 'year = "2011"'
-        loggers = ['rug', 'pu1', 'pu2', 'rad', 'wxt']
-        parameters_rug = ['Ta_200', 'rH_200']
-        parameters_pu1 = ['P_RT_NRT']
-        parameters_pu2 = ['P_RT_NRT_01', 'P_RT_NRT_02', \
-                          'F_RT_NRT_01', 'F_RT_NRT_02']
-        parameters_rad = ['Ta_200', 'rH_200',
+        '''
+        self.loggers = ['rug', 'pu1', 'pu2', 'rad', 'wxt']
+        self.parameters_rug = ['Ta_200', 'rH_200']
+        self.parameters_pu1 = ['P_RT_NRT']
+        self.parameters_pu2 = ['P_RT_NRT_01', 'P_RT_NRT_02', \
+                              'F_RT_NRT_01', 'F_RT_NRT_02']
+        self.parameters_rad = ['Ta_200', 'rH_200',
                           'par_01', 'par_02', 'par_03', 'par_04', 
                           'par_05', 'par_06', 'par_07', 'par_08', 
                           'par_09', 'par_10', 'par_11', 'par_12', 
                           'swdr_13', 'swdr_14', 'swdr_15', 'swdr_16',
                           'swdr_17', 'swdr_18,', 'swdr_19', 'swdr_20', 
                           'swdr_21,', 'swdr_22', 'swdr_23', 'swdr_24']
-        parameters_wxt = ['P_RT_NRT', 'SWDR_300', \
+        self.parameters_wxt = ['P_RT_NRT', 'SWDR_300', \
                       'SWUR_300', 'LWDR_300', 'LWUR_300', 'Ts_10']
+        '''
         par_range = {'Ta_200': [-10,50],'rH_200': [0,100], \
                      'P_RT_NRT': [0,60], \
                      'P_RT_NRT_01': [0,60], 'P_RT_NRT_02': [0,60],
@@ -240,52 +252,70 @@ class VISLevel0050:
                 f.close()
                 r_cmd = "R CMD BATCH " + script + " " + script + ".log"
                 os.system(r_cmd)
+   
+   
+    def visualize_wxt(self):
+        """Visualize station datasets
+        """
+        os.chdir(self.r_filepath)
+        r_source = 'source("print.wxt.ki.strip.R")'
+        r_script = 'print.wxt.ki.strip('
+        r_outputpath = 'outputfilepath = "' + self.output_path + '",'
+        r_pattern = 'ptrn  = "' + self.pattern + '"'
+        par_range = {'Ta_200': [-10,50],'rH_200': [0,100], \
+                     'P_RT_NRT': [0,60], \
+                     'P_RT_NRT_01': [0,60], 'P_RT_NRT_02': [0,60],
+                     'F_RT_NRT_01': [0,60], 'F_RT_NRT_02': [0,60],
+                     'SWDR_300': [0,1400], 'SWUR_300': [0,500], \
+                     'LWDR_300': [200,500], 'LWUR_300': [200,500], \
+                     'WD': [0,360], 'WV': [0,10], \
+                     'Ts_10': [10, 50], \
+                     'p_200': [750, 1100], \
+                     'par_01': [0, 5000], 'par_02': [0, 5000], \
+                     'par_03': [0, 5000], 'par_04': [0, 5000], \
+                     'par_05': [0, 5000], 'par_06': [0, 5000], \
+                     'par_07': [0, 5000], 'par_08': [0, 5000], \
+                     'par_09': [0, 5000], 'par_10': [0, 5000], \
+                     'par_11': [0, 5000], 'par_12': [0, 5000], \
+                     'swdr_13': [0, 5000], 'swdr_14': [0, 5000], \
+                     'swdr_15': [0, 5000], 'swdr_16': [0, 5000], \
+                     'swdr_17': [0, 5000], 'swdr_18': [0, 5000], \
+                     'swdr_19': [0, 5000], 'swdr_20': [0, 5000], \
+                     'swdr_21': [0, 5000], 'swdr_22': [0, 5000], \
+                     'swdr_23': [0, 5000], 'swdr_24': [0, 5000]}
         
-
-    '''        
-        for logger in loggers:
-            r_logger = 'logger = "' + logger + '",'
-            if logger == 'rug':
-                parameters = parameters_rug
-            elif logger == 'wxt':
-                parameters = parameters_wxt
-            elif logger == 'pu1':
-                parameters = parameters_pu1
-            elif logger == 'pu2':
-                parameters = parameters_pu2
-            elif logger == 'rad':
-                parameters = parameters_rad
-            for parameter in parameters:
-                r_prm = 'prm = "' + parameter + '",'
-                r_range = 'range = c(' + str(par_range[parameter][0]) + ', ' + \
-                           str(par_range[parameter][1]) + '),'
-                r_colour = 'colour = VColList$' + str(parameter) + ', '
-                r_fun = 'fun = mean,'
-                if parameter == 'P_RT_NRT':
-                    r_fun = 'fun = sum,'
-                for year in range(2011, 2013):
-                    r_year = 'year = "' + str(year) + '"'
-                    r_cmd = r_source + "\n" + \
-                        r_script + "\n" + \
-                        r_inputfilepath + "\n" + \
-                        r_outputpath + "\n" + \
-                        r_logger + " \n" + \
-                        r_prm + " \n" + \
-                        r_fun + " \n" + \
-                        r_arrange + " \n" + \
-                        r_range + " \n" + \
-                        r_pattern + " \n" + \
-                        r_colour + " \n" + \
-                        r_year + ")\n"
+        for year in range(2011, 2013):
+            self.inputpath = []
+            for i in range(0, len(self.loggers)):
+                for path, dirs, files in os.walk(os.path.abspath(\
+                                         self.tl_processing_path)):
+                        for filename in fnmatch.filter(files, \
+                                                "*" + self.loggers[i] + \
+                                                "*" + str(year) + "*" + \
+                                                self.pattern):
+                            self.inputpath.append(\
+                                            os.path.join(path))
+            self.inputpath = sorted(set(self.inputpath))
+            for station in self.inputpath:
+                r_inputfilepath = 'inputpath = "' + station + '/",'
+                r_year = 'year = "' + str(year) + '",'
+                r_pid = 'plotid = "' + station [-20:-16] + '",'
+                r_cmd = r_source + "\n" + \
+                r_script + "\n" + \
+                r_inputfilepath + "\n" + \
+                r_outputpath + "\n" + \
+                r_pid + " \n" + \
+                r_year + " \n" + \
+                r_pattern + ")\n"
     
-                    script = "vis0050.rscript" 
-                    f = open(script,"w")
-                    f.write(r_cmd)
-                    f.close()
-                    r_cmd = "R CMD BATCH " + script + " " + script + ".log"
-                    print r_cmd
-                    os.system(r_cmd)
-    '''
+                script = "vis0050.rscript" 
+                f = open(script,"w")
+                f.write(r_cmd)
+                f.close()
+                r_cmd = "R CMD BATCH " + script + " " + script + ".log"
+                os.system(r_cmd)
+
+   
     def get_level0005_standards(self):
         """Sets format standards for level 1 station data files
         """
