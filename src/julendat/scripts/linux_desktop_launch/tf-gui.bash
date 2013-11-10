83fe85fe6dede228d9919b7ff7815cbb1b991351
tnauss <tnauss@localhost>
2013-11-10 16:05:36 +0100

    Update: Add .desktop scripts for Linux double-click launch.

---
+++ src/julendat/scripts/linux_desktop_launch/tf-gui.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PYTHONPATH=/media/permanent/development/released/googlecode/julendat/src
+cd /media/permanent/development/released/googlecode/julendat/src/julendat/scripts/stations_ki
+python ki_process_uitfstation_level0000_gui.py
