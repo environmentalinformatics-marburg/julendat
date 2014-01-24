8ce2ede97bfd2b66f2a434aa968b139ce5c78925
sforteva <sforteva@localhost>
2014-01-24 10:57:14 +0100

    Bugfix: Fix bug in quality check

--- src/julendat/rmodules/write.aggregate.ki.data.R
+++ src/julendat/rmodules/write.aggregate.ki.data.R
@@ -2,6 +2,7 @@ write.aggregate.ki.data <- function(inputfilepath,
                                     outputfilepath,
                                     level = "1h",
                                     start.column = 9,
+                                    detail = FALSE,
                                     ...) 
 {
 
@@ -10,14 +11,16 @@ write.aggregate.ki.data <- function(inputfilepath,
   out <- aggregate.ki.data(inputfilepath,
                            level = level,
                            start.column = start.column,
+                           detail = detail, 
                            ...)
   
-  print(str(out))
-  print(tail(out))
+ # print(str(out))
+ # print(tail(out))
   print(outputfilepath)
-  write.table(out, outputfilepath, sep = ",", col.names = T, row.names = F)
+ write.table(out, file = outputfilepath, sep = ",", col.names = T, row.names = F)
   
 }
+
 
 #write.aggregate.ki.data("/media/permanent/development/test/julendat/processing/plots/ki/0000cof3/ca05_cti05_0050/ki_0000cof3_000rug_201111010000_201111302355_eat_ca05_cti05_0050.dat",
 #                       "/media/permanent/development/test/julendat/processing/plots/ki/0000cof3/ca05_cti05_0050/test.dat",
