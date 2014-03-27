cb7d2ed85c5fa44524fd8250409f8a873ed39ee9
jsonne <jsonne@localhost>
2014-03-27 09:34:22 +0100

    update: add check script

--- src/julendat/scripts/check/revision_level_310_manual.R
+++ src/julendat/scripts/check/revision_level_310_manual.R
@@ -1,17 +1,31 @@
 rm(list=ls())
 
 # Set path environment
-location = "AEG"
-setwd("D:/active/bexis/level_0310_hourly/processing/")
+location = "SEG"
+setwd("/media/jsonne/Volume/bexis/level_0310_hourly_reloaded/manual/")
 revision.file <- paste(location, "_revised_reloaded.csv",sep = "")
 
 # Functions
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
+# Copy files to working folder
+act.path <- paste(location, "_revised/be", sep = "")
+act.files <- list.files(path = act.path,
+                        pattern = ".dat",
+                        full.names = TRUE, recursive = TRUE)
+sapply(act.files, function(x){
+  act.file <- x
+  out.file <- gsub(paste(location, "revised", sep = "_"), 
+                   paste(location, "revised_reloaded", sep = "_"), act.file)
+  dir.create(dirname(out.file), recursive = TRUE, showWarnings = TRUE)
+  file.copy(act.file, out.file)
+})
+
 # Read revision information
 revision.info <- read.csv(revision.file, header = TRUE, sep = ",")
-
+#revision.info <- subset(revision.info, revision.info$PlotId == "AEG34")
 # Revise data
+#x<-1
 sapply(seq(1, nrow(revision.info)), function(x){
   act.plotid <- paste("000", revision.info$PlotId[x], sep = "")
   act.parameter <- as.character(revision.info$Parameter[x])
@@ -19,27 +33,32 @@ sapply(seq(1, nrow(revision.info)), function(x){
   act.range <- trim(as.character(revision.info$Range[x]))
   if(grepl("-", act.range)){
     sep <- gregexpr(pattern = "-", act.range)[[1]][1]
-    act.range <- seq(substr(act.range, 1, sep-1), 
-                     substr(act.range, sep+1, nchar(act.range)))
+    act.range <- seq(as.numeric(substr(act.range, 1, sep-1)),
+                     as.numeric(substr(act.range, sep+1, nchar(act.range))))
   } else {
     act.range <- as.numeric(act.range)
   }
-  act.path <- paste(location, "_revised/be/", 
-                    act.plotid, "/gc02_fah01_0310/", sep = "")
-  act.file <- list.files(path = act.path, 
+  act.path <- paste(location, "_revised_reloaded/be/",
+                    act.plotid, "/gc02_fah01_0310", sep = "")
+  act.file <- list.files(path = act.path,
                          pattern = paste("_", act.year, sep = ""),
                          full.names = TRUE, recursive = TRUE)
-  out.file <- gsub("AEG_revised", "AEG_revision_reloaded", act.file)
-  if(file.exists(out.file)){
-    act.file <- out.file
-  }
+#   out.file <- gsub(paste(location, "revised", sep = "_"), 
+#                    paste(location, "revised_reloaded", sep = "_"), act.file)
+  print(act.file)
+#   print(out.file)
+  print(act.parameter)
+#   if(!file.exists(out.file)){
+#     dir.create(dirname(out.file), recursive = TRUE, showWarnings = TRUE)
+#     file.copy(act.file, out.file)
+#   }
   act.data <- read.csv(act.file, header = TRUE, sep = ",")
   act.col <- which(colnames(act.data) == act.parameter)
   for(i in seq(1, length(act.range))){
     act.datetime <- paste(act.year, sprintf("%02d", act.range[i]), sep = "-")
     act.data[grep(act.datetime, act.data$Datetime), ][, act.col] <- NaN
   }
-  dir.create(dirname(out.file), recursive = TRUE, showWarnings = FALSE)
-  write.table(act.data, out.file, sep = ",", row.names = FALSE)
+  write.table(act.data, act.file, sep = ",", row.names = FALSE)
 })
+
 
