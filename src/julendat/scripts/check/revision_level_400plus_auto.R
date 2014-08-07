97c71f3fd09ffa98d089041f5ad030963a790f68
tnauss <tnauss@localhost>
2014-08-07 12:14:32 +0200

    Update: Adapt script for all level 400 plus.

--- src/julendat/scripts/check/revision_level_400plus_auto.R
+++ src/julendat/scripts/check/revision_level_400plus_auto.R
@@ -1,8 +1,8 @@
 rm(list=ls())
 
 # Set path environment
-setwd("/media/permanent/active/bexis/processing")
-level = "_0420.dat"
+setwd("/media/memory01/ei_data_exploratories/processing/plots/")
+level = "_0400.dat"
 
 # Set threshold values
 switch(level,
@@ -15,17 +15,44 @@ switch(level,
 parameters <- c("Ta_200", "Ta_10", "rH_200",
                 "Ts_5", "Ts_10", "Ts_20", "Ts_50")
 precipitation <- c("P_RT_NRT", "P_container_RT", "P_container_NRT")
-act.path <- "revised_reloaded_level_0310_plus"
+act.path <- "revised_reloaded_0400plus/"
 act.files <- list.files(path = act.path,
                         pattern = level,
                         full.names = TRUE, recursive = TRUE)
 
-x <- act.files[1]
-sapply(act.files, function(x){
+act.files.vip <- Filter(function(x) grepl("(E|A)EMU", x), act.files)
+act.files.cemu <- Filter(function(x) grepl("CEMU", x), act.files)
+
+# revision for all CEMU-files
+sapply(act.files.cemu, function(x){
   act.file <- x
   print(act.file)
   act.data <- read.table(act.file, header = TRUE, sep = ",")
-  i <- 1
+  for(i in seq(1, length(parameters))){
+    act.parameter <- parameters[i]
+    act.col <- which(colnames(act.data) == act.parameter)
+    check.col <- which(colnames(act.data) == paste(act.parameter, "nan", 
+                                                   sep ="_"))
+    if(nrow(act.data[act.data[, check.col] > thv, ]) > 0){
+      print("replacing...")
+      for(c in seq(check.col - 8, check.col)){
+        print(c)
+        act.data[act.data[, check.col] > thv, ][, c] <- NaN  
+      }
+    }
+  }
+  
+  out.file <- gsub("revised_reloaded_level_0310_plus", 
+                   "revised_reloaded_level_0400_plus", act.file)
+  dir.create(dirname(out.file), recursive = TRUE, showWarnings = FALSE)
+  write.table(act.data, out.file, sep = ",", row.names = FALSE)
+})
+
+# revision for all AEMU-/EEMU-files
+sapply(act.files.vip, function(x){
+  act.file <- x
+  print(act.file)
+  act.data <- read.table(act.file, header = TRUE, sep = ",")
   for(i in seq(1, length(parameters))){
     act.parameter <- parameters[i]
     act.col <- which(colnames(act.data) == act.parameter)
@@ -45,11 +72,16 @@ sapply(act.files, function(x){
     act.col <- which(colnames(act.data) == act.parameter)
     check.col <- which(colnames(act.data) == paste(act.parameter, "nan", 
                                                    sep ="_"))
-    if(length(check.col) > 0){
+    #The following three lines are necessary in case of "NA"-values in the columns "P_RT_NRT_nan",
+    #"P_container_RT_nan" and "P_container_NRT_nan"
+    #if(is.na(act.data[, check.col])){
+    #  act.data[, check.col] <- 9999
+    #}
+    if(nrow(act.data[act.data[, check.col] > thv, ]) > 0){
       print("replacing...")
       for(c in seq(check.col - 8, check.col)){
         print(c)
-        act.data[, c] <- NaN  
+        act.data[act.data[, check.col] > thv, ][, c] <- NaN  
       }
     }
   }
@@ -59,3 +91,5 @@ sapply(act.files, function(x){
   dir.create(dirname(out.file), recursive = TRUE, showWarnings = FALSE)
   write.table(act.data, out.file, sep = ",", row.names = FALSE)
 })
+
+
