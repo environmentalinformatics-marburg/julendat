9b1ed7f978dc9ca2f40da14a1d89e012376a6dde
josephine sonnenberg <josephine.sonnenberg@geo.uni-marburg.de>
2014-08-11 07:36:36 +0000

    added a necessary line for level 0420

--- src/julendat/scripts/bexis_upload_aggregated/bexis_upload_aggregated_.R
+++ src/julendat/scripts/bexis_upload_aggregated/bexis_upload_aggregated_.R
@@ -1,10 +1,19 @@
-setwd("/media/dogbert/dev/be/julendat/processing/plots/")
+rm(list=ls())
 
-level = "0400"
-scr_path = "/media/dogbert/dev/be/julendat/processing/"
-new_path ="/media/dogbert/dev/be/julendat/processing/"
-files <- list.files(paste0(path,"plots/"), pattern=glob2rx(paste0("*", level,".dat")), recursive=TRUE, full.names=TRUE)
+# just adjust the following four parameters and, in case of editing level 0420, uncomment line 25
+level = "0420" # "0400", "0405" or "0420"
+region = "ALB" # "ALB", "HAI" or "SCH"
+station = "AEMU_EEMU" # "CEMU" or "AEMU_EEMU"
+date = "20140811" # date of today
 
+setwd(paste("/media/dogbert/DEV/ready_for_bexis/eipastprocessing/", level, "/", region, "_",
+            station, "_",level, "_original", sep=""))
+
+scr_path = paste("/media/dogbert/DEV/ready_for_bexis/eipastprocessing/", level, "/", region, "_",
+                 station, "_",level, "_original", sep="")
+new_path = paste("/media/dogbert/DEV/ready_for_bexis/eipastprocessing/", level, "/", region, "_",
+                station, "_",level, "_", date, sep="")
+files <- list.files(pattern=paste("_", level ,".dat" ,sep=""), recursive=TRUE, full.names=TRUE)
 
 dat.list <- lapply(files, function(i) {
   dat <- read.table(i, header = T, sep = ",", stringsAsFactors = F)
@@ -13,6 +22,7 @@ dat.list <- lapply(files, function(i) {
 })
 
 for (activ_file in dat.list){
+  #activ_file$Datetime <- paste(activ_file$Datetime, "-01-01 01:00:00", sep="") # only for level 0420
   year <- substr(as.Date(activ_file$Datetime[1]), 1,4)
 
   plotId.head <- substr(activ_file$PlotId[1], 4,6)
@@ -24,10 +34,9 @@ for (activ_file in dat.list){
     plotId.numbers<- substr(activ_file$PlotId, 8,8)
   }
   activ_file$PlotId <- paste0(plotId.head, plotId.numbers)
-  write.csv(activ_file, paste(new_path, paste0(plotId.head, plotId.numbers[1],"_",level,"_",stationId,"_", year,".txt"), sep=""), row.names=F, na="NA")
-  
+  write.csv(activ_file, paste(new_path, paste0("/", plotId.head, plotId.numbers[1],"_",level,"_",stationId,"_", year,".txt"),
+                              sep=""), row.names=F, na="NA")
 }
-
 
 
   
